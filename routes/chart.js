c6cb6c86372f31dfdd6890f9909b0ae569934146
cheully <cag42@njit.edu>
2015-04-13 13:19:31 -0400

    up

--- routes/chart.js
+++ routes/chart.js
@@ -1,29 +1,35 @@
 exports.bargraph = function(req, res) {
-	
+	// Connecting to the mongodb database with the information from the institution document
 	var dburl = 'localhost:27017/IPEDS_Documentation';
 	var collection = ['institutions'];
 	var db = require('mongojs').connect(dburl, collection);
-
+	
+	// Get all the institutions' specified fields and sort them based on Total Enrollment in descending order
 	db.institutions.find({}, {"UNITID": 1, "INSTNM": 1, "GENDER.Total Enrollment":1}).sort({"GENDER.Total Enrollment": -1}, function(err, info) {
-		var list = new Array();
+
+		var list = new Array(); // Array to store the top 10 institutions
 		
 		for (var i in info) {
-			if (i != 9)
+			if (i != 10) // Stop at the 11th object
 			{
-				var obj = new Object();
+				var obj = new Object(); // Object that will store the edited key-value pairs
+				
 				for (var key in info[i]) {
 					if (key === "GENDER") {
+						// Recognizing the stored objects in info and converting it to integer
 						obj["Total"] = parseInt(JSON.stringify(info[i][key][0]["Total Enrollment"]));
 					} else {
 						obj[key] = info[i][key];
 					}
 				}
-				list.push(obj);
+				list.push(obj); // Put new object into list
 				
 			} else
 				break;
 		}
-		console.log(list);
+		
+		//console.log(list);
+		// Render bargraph with the title and lists
 		res.render('bargraph', {title: 'Bar Graph', topSchools: list});
 	});
 };
