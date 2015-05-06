e9a3d2f025611620584c6f8245e95ab7fcc716b3
cheully <cag42@njit.edu>
2015-05-05 23:47:06 -0400

    changrs

--- routes/add.js
+++ routes/add.js
@@ -8,9 +8,9 @@ exports.additionalfiles = function(req, res) {
 };
 
 exports.additional_success = function( req, res, next) {
-	/*for (var i=0; i<req.files.csvFile.length; i++){
-	console.log(req.files.csvFile[i].name);
-	}*/
+	for (var i=0; i<req.files.csvFile.length; i++){
+		console.log(req.files.csvFile[i].name);
+	}
 
 	var fs = require('fs');
 	var fileName = req.files.csvFile.name;
@@ -108,5 +108,78 @@ exports.additional_success = function( req, res, next) {
 	});
 	//res.render('addToDB_success', {title: 'Database Update Success!', location:'/'});
 	}
+};
+
+
+exports.additional_success = function( req, res, next) {
+
+	var fs = require('fs');
+	var fileName = req.files.csvFile.name;
+	var year = '20' + fileName.substring(3,5);
+			
+		// get the temporary location of the file
+		var tmp_path = req.files.csvFile.path;
+		// set where the file should actually exists - in this case it is in the "incoming" directory
+		var target_path = './incoming/' + fileName;
+		// move the file from the temporary location to the intended location
+		fs.rename(tmp_path, target_path, function(err) {
+			if (err) throw err;
+			// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+			fs.unlink(tmp_path, function() {
+				if (err) throw err;
+				//res.render('upload_success', {title: 'Success!'} );
+			});
+		});
+		
+	var csv = require('csv'),
+		fs = require('fs'),
+		records = new Array(),
+		records = [];
+		
+	csv(records)
+		.from.stream(fs.createReadStream('./incoming/' + fileName), {
+		
+		columns: true
+	})
+		.on('record', function (row, index) {
+		records.push(row);
+	})
+		.on('end', function (count) {
+		var MongoClient = require('mongodb').MongoClient;
+	   // Connect to the db
+		MongoClient.connect("mongodb://localhost:27017/IPEDS_Documentation", function (err, db) {
+			if(err) throw err;
+			
+			var collection = db.collection('institutions');
+			
+			for(var i in records){
+				
+				var totalTuitions = parseInt(records[i]['F1B01']) +  parseInt(records[i]['F1E08']);
+				
+				collection.find({$and:[{'UNITID': records[i]['UNITID']}, {'TUITION':{$exists:true}} ]}).count(function(err, value) {
+					
+					var obj = new Object();
+					obj[year] = totalTuitions;
+					
+					//console.log(value);
+					if (parseInt(value) === 0) {
+						collection.update({'UNITID': records[i]['UNITID']}, {$push:{'TUITION':obj}}, function(err, done) {
+								//console.log('Created and saved!');
+						});
+							
+					} else {
+						var name = 'TUITION.'+year;
+						collection.update({'UNITID': records[i]['UNITID']}, {$set:{ name:totalTuitions}}, { upsert: true }, function(err, done) {
+								console.log('Saved!');
+						});
+						
+					}
+	
+				});		
+			}
+		});
+	});
+	
+	//res.render('addToDB_success', {title: 'Database Update Success!', location:'/'});
 };
 
